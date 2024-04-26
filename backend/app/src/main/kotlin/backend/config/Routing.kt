2f2f054c0dfb274c8b9306443887949cd1c54ce6
Joakim Valand <joakim.valand@itera.com>
2024-04-26 17:26:04 +0200

    wip: feat: add email claim and /userinfo endpoint

--- backend/app/src/main/kotlin/backend/config/Routing.kt
+++ backend/app/src/main/kotlin/backend/config/Routing.kt
@@ -5,6 +5,7 @@ import backend.repository.SpeakerRepository
 import backend.repository.UserRepository
 import backend.repository.WorkshopRepository
 import backend.route.*
+import backend.service.UserService
 import backend.service.WorkshopService
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -16,16 +17,17 @@ fun Application.configureRouting() {
     val adminRepository = AdminRepository()
     val workshopRepository = WorkshopRepository()
     val speakerRepository = SpeakerRepository()
-    val workshopService = WorkshopService(environment.config)
+    val workshopService = WorkshopService(environment.config, workshopRepository, speakerRepository)
+    val userService = UserService(environment.config, workshopService, userRepository)
 
     configureAuth0Route(userRepository)
-    configureUserRoutes(userRepository)
-    configureWorkshopRoutes(workshopRepository, speakerRepository, workshopService)
+    configureUserRoutes(userService)
+    configureWorkshopRoutes(workshopService)
     configureAdminRoutes(adminRepository)
     configureApiRoutes()
 
     routing {
-        authenticate ("auth0-user") {
+        authenticate("auth0-user") {
             get("/auth") {
                 call.respondText("Hello World!")
             }
--- backend/app/src/main/kotlin/backend/route/WorkshopRoute.kt
+++ backend/app/src/main/kotlin/backend/route/WorkshopRoute.kt
@@ -1,35 +1,26 @@
 package backend.route
 
 import backend.service.WorkshopService
-import backend.repository.SpeakerRepository
-import backend.repository.WorkshopRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureWorkshopRoutes(workshopRepository: WorkshopRepository,
-                                        speakerRepository: SpeakerRepository,
-                                        workshopService: WorkshopService
+fun Application.configureWorkshopRoutes(
+    workshopService: WorkshopService
 ) {
     routing {
-        workshopRoute(workshopRepository, speakerRepository, workshopService)
+        workshopRoute(workshopService)
     }
 }
 
-fun Routing.workshopRoute(workshopRepository: WorkshopRepository,
-                          speakerRepository: SpeakerRepository,
-                          workshopService: WorkshopService
+fun Routing.workshopRoute(
+    workshopService: WorkshopService
 ) {
     authenticate("auth0-user") {
         get("/workshop") {
-            val speakers = speakerRepository.list()
-                .groupBy ({ it.workshopId }, {it.toDTO()} )
-
-            val workshops = workshopRepository.list().map { it.toDTO(speakers[it.id])  }
-
-            call.respond(workshops)
+            call.respond(workshopService.getWorkshops())
         }
     }
     authenticate("auth0-admin") {
